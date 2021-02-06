385414dc7e613272b3515f09a2069ed513f32202
Jacob Hendershott <jwhendershott@gmail.com>
2021-02-06 16:33:57 -0600

    created server.js and sql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+insert into burgers (burger_name) values ('Big Kahuna Burger');
+insert into burgers (burger_name) values ('Krusty Burger');
+insert into burgers (burger_name) values ("Mooby's Burger");
