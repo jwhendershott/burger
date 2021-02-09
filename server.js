ec568a300acc2966abee3c75f900de742c472cb2
Jacob Hendershott <jwhendershott@gmail.com>
2021-02-08 19:59:13 -0600

    updated config, controllers, & views/added models & partials

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const exphbs = require('expreess-handlebars');
-const mysql = require('mysql');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 8080;
 
@@ -11,8 +10,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require('express-handlebars');
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
@@ -20,4 +17,4 @@ const routes = require('./controllers/burgerController.js');
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
+app.listen(PORT, () => console.log(`App now listening at http://localhost:${PORT}`));
