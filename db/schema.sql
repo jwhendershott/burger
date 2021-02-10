493223a90437a523c51c9a21d6171607b6443ff4
Jacob Hendershott <jwhendershott@gmail.com>
2021-02-09 20:34:42 -0600

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,6 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(50) NOT NULL,
     devoured boolean,
 	PRIMARY KEY (id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
 insert into burgers (burger_name) values ('Big Kahuna Burger');
-insert into burgers (burger_name) values ('Krusty Burger');
-insert into burgers (burger_name) values ("Mooby's Burger");
+insert into burgers (burger_name) values ('Royale With Cheese');
+insert into burgers (burger_name) values ("Le Big Mac");
